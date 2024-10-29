0994eda2380673f38efd954998c94f3e358e8324
IchigoHatake <marioms1988@hotmail.com>
2024-10-29 16:06:15 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sun Pictures Project
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Kimtaro88/pen/JjgLyJg](https://codepen.io/Kimtaro88/pen/JjgLyJg).
+
---
+++ src/script.js
@@ -0,0 +1,44 @@
+var viewer = OpenSeadragon({
+    id: "seadragon-viewer",
+    prefixUrl: "//openseadragon.github.io/openseadragon/images/",
+    tileSources: [{
+            type: 'legacy-image-pyramid',
+            levels: [{
+                url: 'https://eopro.esa.int/wp-content/uploads/2024/10/PHI_Visible-light_20230322.jpg',
+                width:  9600,
+                height: 9600
+            }]
+        },{
+            type: 'legacy-image-pyramid',
+            levels: [{
+                url: 'https://eopro.esa.int/wp-content/uploads/2024/10/PHI_Magnetogram_20230322.jpg',
+                width:  9600,
+                height: 9600
+            }]
+        },{
+            type: 'legacy-image-pyramid',
+            levels: [{
+                url: 'https://eopro.esa.int/wp-content/uploads/2024/10/PHI_Velocity-map_20230322.jpg',
+                width:  9600,
+                height: 9600
+            }]
+        },{
+            type: 'legacy-image-pyramid',
+            levels: [{
+                url: 'https://eopro.esa.int/wp-content/uploads/2024/10/EUI_Ultraviolet-light_20230322.jpg',
+                width:  9600,
+                height: 9600
+            }]
+        }],
+
+        sequenceMode: true, //next page, prev page
+        showReferenceStrip: true,
+        //referenceStripScroll: 'vertical', //Vertical Scrolling Image Reference Strip
+        // showRotationControl: true, //button rotate
+        showNavigator: true,
+        maxZoomPixelRatio: 5.0,
+        controlsFadeDelay: 3500,
+        homeFillsViewer: false
+});
+
+
